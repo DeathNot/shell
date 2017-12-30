0957d3fa1788bd48c8496fa97cf6a58da8ddeb9a
DeathNot <725509787@qq.com>
2017-12-30 19:37:26 +0800

    test

---
+++ day2/test.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+var=100
+var2=50
+var3=45
+var4=$[$var * ($var2 - $var3)]
+echo the final result is $var4
---
+++ day2/test2.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+var=$(echo "scale=4; 3.25 / 2" | bc)
+echo the result is $var
